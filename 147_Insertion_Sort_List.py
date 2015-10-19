4392400a38e92446690f93f693126254e45725b1
jessie-JNing <jessie.JNing@gmail.com>
2015-10-19 20:43:35 -0300

    sort_link_list

--- 147_Insertion_Sort_List.py
+++ 147_Insertion_Sort_List.py
@@ -20,7 +20,24 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        if head:
+            guard = ListNode(0)
+            current = head
+            previous = guard
+            while current:
+                temp = current.next
+                while previous.next and previous.next.val < current.val:
+                    previous = previous.next
 
+                current.next = previous.next
+                previous.next = current
+
+                previous = guard
+                current = temp
+
+            return guard.next
+        else:
+            return head
 
     def insertionSort(self, nums):
         if len(nums) < 2:
@@ -37,7 +54,25 @@ class Solution(object):
             return nums
 
 
+
 if __name__=="__main__":
 
     test = Solution()
     print test.insertionSort([2,7,9,3,1])
+
+    a1 = ListNode(5)
+    a2 = ListNode(2)
+    a3 = ListNode(3)
+    a4 = ListNode(7)
+    a5 = ListNode(1)
+    a1.next = a2
+    a2.next = a3
+    a3.next = a4
+    a4.next = a5
+
+    head_node = test.insertionSortList(a1)
+
+
+    while head_node:
+        print head_node.val, "->",
+        head_node = head_node.next
